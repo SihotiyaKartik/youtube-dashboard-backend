83cbf95485be45e8ff3621ba1277b136e6d2c5f5
Kartik Sihotiya <sihotiyak.ug19.ec@nitp.ac.in>
2023-08-10 12:43:51 +0530

    User registration

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, BadRequestException } from '@nestjs/common';
 import { UserService } from './user.service';
+import { UserDto } from './user.dto';
 
-@Controller('user')
+@Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @Post('register')
+  async register(@Body() createUserDto: UserDto): Promise<{ message: string }> {
+    const { email, password } = createUserDto;
+    const isOldUser = await this.userService.findUserByEmail(email);
+    if (isOldUser) {
+      throw new BadRequestException('Account already exist, Use another email');
+    }
+    await this.userService.createUser(email, password);
+    return { message: 'Account is registered successfully' };
+  }
+
+  //   @Post('login')
+  //   async login(@Body() loginUserDto: UserDto): Promise<{message: string}> {
+  //     const {email, password} = loginUserDto;
+  //   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,6 +2,16 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from 'src/entity/user.entity';
 import { Repository } from 'typeorm';
+import * as bcrypt from 'bcrypt';
+
+function extractUsernameFromEmail(email: string): string {
+  const splitIndex = email.indexOf('@');
+  if (splitIndex !== -1) {
+    return email.substring(0, splitIndex);
+  } else {
+    throw new Error('Invalid email while extracting username from it');
+  }
+}
 
 @Injectable()
 export class UserService {
@@ -9,4 +19,17 @@ export class UserService {
     @InjectRepository(User)
     private readonly userRepository: Repository<User>,
   ) {}
+
+  async createUser(email: string, password: string): Promise<User> {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = new User();
+    user.email = email;
+    user.username = extractUsernameFromEmail(email);
+    user.password = hashedPassword;
+    return this.userRepository.save(user);
+  }
+
+  async findUserByEmail(email: string): Promise<User | undefined> {
+    return this.userRepository.findOne({ where: { email } });
+  }
 }
