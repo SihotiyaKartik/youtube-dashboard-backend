35a0937234257b0269e53a54ab9c48463405ecc2
Kartik Sihotiya <sihotiyak.ug19.ec@nitp.ac.in>
2023-08-10 11:25:57 +0530

    Settig up controller, module, and service for User entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { VideoModule } from './video/video.module';
+import { UserModule } from './user/user.module';
 import typeOrmConfig from './typeorm.config';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConfig), VideoModule],
+  imports: [TypeOrmModule.forRoot(typeOrmConfig), VideoModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
